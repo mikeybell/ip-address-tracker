2b7c6c9789cf3dd4ad3e20c7afce2e71b2e71e48
Mike Bell <mike.bell@staydomio.com>
2020-11-30 17:05:38 -0500

    chore: gets ip address fetch working

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from './header.module.css';
 
+const URL = `https://geo.ipify.org/api/v1?apiKey=${process.env.REACT_APP_IP_API_KEY}&ipAddress=142.116.168.212`;
+
+const getIpAddress = async () => {
+  try {
+    const res = await fetch(URL);
+    if (!res.ok) throw new Error();
+
+    const data = await res.json();
+    console.log(data);
+    return data;
+  } catch (err) {
+    console.log("ERROR", err)
+  }
+}
+
 export const Header = () => {
+
+  useEffect(() => {
+    getIpAddress();
+  }, [])
+
   return (
     <header className={styles.header}>
       <h1 className={styles.title}>IP Address Tracker</h1>
@@ -13,19 +33,19 @@ export const Header = () => {
       <div className={styles.results}>
         <div className={styles.result}>
           <h2 className={styles.resultTitle}>IP Address</h2>
-          <p>Result</p>
+          <p className={styles.resultValue}>Result</p>
         </div>
         <div className={styles.result}>
           <h2 className={styles.resultTitle}>Location</h2>
-          <p>Result</p>
+          <p className={styles.resultValue}>Result</p>
         </div>
         <div className={styles.result}>
           <h2 className={styles.resultTitle}>Timezone</h2>
-          <p>Result</p>
+          <p className={styles.resultValue}>Result</p>
         </div>
         <div className={styles.result}>
           <h2 className={styles.resultTitle}>ISP</h2>
-          <p>Result</p>
+          <p className={styles.resultValue}>Result</p>
         </div>
       </div>
     </header>
