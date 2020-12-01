663656f4c4126bd77c87d0bd83d6b3204a796d38
Mike Bell <mike.bell@staydomio.com>
2020-12-01 13:07:33 -0500

    chore: map marker

--- src/components/Map/constants.ts
+++
@@ -1,1 +0,0 @@
-export const NEW_YORK_CITY: [number, number] = [-73.96625, 40.78343];
--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import mapboxgl from 'mapbox-gl';
-import { NEW_YORK_CITY } from './constants';
+import { useInitialIpAddress } from '../Header/hooks/useInitialIpAddress';
 import styles from './map.module.css';
 
 mapboxgl.accessToken =
@@ -8,8 +8,16 @@ mapboxgl.accessToken =
     ? process.env.REACT_APP_MAPBOX_TOKEN
     : '';
 
+const marker = document.createElement('div');
+marker.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="46" height="56"><path fill-rule="evenodd" d="M39.263 7.673c8.897 8.812 8.966 23.168.153 32.065l-.153.153L23 56 6.737 39.89C-2.16 31.079-2.23 16.723 6.584 7.826l.153-.152c9.007-8.922 23.52-8.922 32.526 0zM23 14.435c-5.211 0-9.436 4.185-9.436 9.347S17.79 33.128 23 33.128s9.436-4.184 9.436-9.346S28.21 14.435 23 14.435z"/></svg>';
+
 export const Map = () => {
+  const { payload } = useInitialIpAddress();
   const mapContainer = useRef(null);
+
+  useEffect(() => {
+    setupMap([payload.location.lng, payload.location.lat]);
+  }, [payload]);
 
   const setupMap = (center: [number, number]): void => {
     const map = new mapboxgl.Map({
@@ -22,25 +30,11 @@ export const Map = () => {
 
     const nav = new mapboxgl.NavigationControl();
     map.addControl(nav);
+
+    new mapboxgl.Marker(marker)
+      .setLngLat(center)
+      .addTo(map);
   }
-
-  useEffect(() => {
-    const successLocation = (
-      position: { coords: { longitude: number, latitude: number }}
-    ): void => {
-      const { longitude, latitude } = position.coords;
-      setupMap([longitude, latitude]);
-    };
-
-    const errorLocation = (): void => setupMap(NEW_YORK_CITY);
-
-    navigator.geolocation.getCurrentPosition(
-      successLocation,
-      errorLocation,
-      { enableHighAccuracy: true }
-    );
-  }, []);
-
 
   return <div ref={mapContainer} className={styles.map}></div>;
 }
